8f16a07cb9c2b83f5fc66f6f065a17124ebb2c66
Jascha <jascha@jaeh.at>
2019-05-01 04:17:03 +0200

    add links to list headers, format

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ module.exports = {
     logotext: '@magic',
     logo: '/logo.png',
     menu: [
-      { to: '/#magic-modules', text: 'magic-modules'},
-      { to: '/#magic-themes', text: 'magic-themes'},
-      { to: '/#magic-client', text: 'magic-client'},
-    ]
-  }
+      { to: '/#magic-modules', text: 'magic-modules' },
+      { to: '/#magic-themes', text: 'magic-themes' },
+      { to: '/#magic-client', text: 'magic-client' },
+    ],
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
 module.exports = () => [
   h2('@magic'),
   p('magic is a module/component based generator for static pages and serverless deployments.'),
-  p('goal of the project is to keep the client boilerplate footprint minimal yet easily extendable'),
+  p(
+    'goal of the project is to keep the client boilerplate footprint minimal yet easily extendable',
+  ),
 
   GitBadges({
     project: 'magic/core',
     appveyor: 'jaeh/core',
   }),
 
-  h2({ id: 'magic-modules' }, '@magic-modules'),
+  h2({ id: 'magic-modules' }, Link({ to: 'https://github.com/magic-modules/' }, '@magic-modules')),
   p([
     'the building blocks of @magic are modules that provide components.',
     ' @magic understands html5, css3 and ecmascript 6.',
   ]),
   ModuleList,
 
-
-  h2({ id: 'magic-themes' }, '@magic-themes'),
+  h2({ id: 'magic-themes' }, Link({ to: 'https://github.com/magic-modules/' }, '@magic-themes')),
   p([
     '@magic-themes are css-in-js files prepared for usage in @magic.',
     ' themes allow easy customization of your @magic app',
@@ -24,7 +25,7 @@ module.exports = () => [
 
   ThemeList,
 
-  h2({ id: 'magic-client' }, '@magic-client'),
+  h2({ id: 'magic-client' }, Link({ to: 'https://github.com/magic-modules/' }, '@magic-client')),
   p([
     '@magic-client libraries are scripts that provide maximal functionality with minimal footprint.',
   ]),
