2ae69d0c373db7c388c70520b01bf889d7b992c4
Jascha <jascha@jaeh.at>
2019-05-05 03:21:35 +0200

    update docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 module.exports = () => [
-  h2('@magic'),
+  h1('@magic'),
   p('magic is a module/component based generator for static pages and serverless deployments.'),
   p(
     'goal of the project is to keep the client boilerplate footprint minimal yet easily extendable',
   ),
 
+  h2('git badges'),
   GitBadges({
     project: 'magic/core',
     appveyor: 'jaeh/core',
   }),
+
+  h3('plugins'),
 
   ModuleList,
 
