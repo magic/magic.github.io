1d373c0c3194de4fcffb385875bbb1be3a418d24
Jascha <jascha@jaeh.at>
2019-05-08 15:47:38 +0200

    format

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,4 @@
 const { is } = require('@magic/test')
 const app = require('../src/app.js')
 
-module.exports = [
-  { fn: () => app, expect: is.object },
-]
+module.exports = [{ fn: () => app, expect: is.object }]
