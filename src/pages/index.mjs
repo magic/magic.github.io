2281d0e3c78c72bbef63c8db62d2bafff37b5193
Jascha <jascha@jaeh.at>
2019-05-20 12:56:50 +0200

    update docs

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -1,5 +1,5 @@
 export const View = () => [
-  h1('@magic'),
+  h1([Link({ to: 'https://magic.github.io' }, '@magic')]),
   p('magic is a module/component based generator for static pages and serverless deployments.'),
   p(
     'goal of the project is to keep the client boilerplate footprint minimal yet easily extendable',
