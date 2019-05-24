8e124d5d9023c6ed6d722d3bdbe96efb093b3503
Jascha <jascha@jaeh.at>
2019-05-24 22:02:10 +0200

    fix page index link

--- src/pages/index.mjs
+++ src/pages/index.mjs
@@ -1,5 +1,5 @@
 export const View = () => [
-  h1([Link({ to: 'https://magic.github.io' }, '@magic')]),
+  h1([Link({ to: '/' }, '@magic')]),
   p('magic is a module/component based generator for static pages and serverless deployments.'),
   p(
     'goal of the project is to keep the client boilerplate footprint minimal yet easily extendable',
