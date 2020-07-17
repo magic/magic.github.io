209ab0a7093931357c0eb26e5ed6cb0d31bb0714
jascha <jascha@jaeh.at>
2020-07-17 21:35:01 +0200

    app: add seo

--- src/app.mjs
+++ src/app.mjs
@@ -3,6 +3,20 @@ export const state = {
   description: '@magic static and serverless webapp framework.',
   branding: '@magic',
 
+  seo: {
+    name: '@magic',
+    url: `https://magic.github.io/`,
+    about: 'magic is a jamstack generator.',
+    image: `https://magic.github.io/magic-preview.jpg`,
+    author: {
+      '@type': 'person',
+      name: 'Jascha Ehrenreich',
+      jobTitle: 'Technomancer',
+      image: 'https:/jaeh.at/img/jascha.ehrenreich.jpg',
+      url: 'https://jaeh.at',
+    },
+  },
+
   menu: [
     { to: '/', text: 'about' },
     { to: '/getting-started/', text: 'usage' },
